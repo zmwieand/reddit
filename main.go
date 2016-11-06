365a8dae6db6570995061181a0c8e0cb5a589a09
zmwieand <zmwieand@gmail.com>
2016-11-06 01:37:47 -0500

    dynamically adds in the command line flags to the program

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
+import "io/ioutil"
 import "flag"
 import "fmt"
 import "github.com/PuerkitoBio/goquery" 
+import "strings"
 
 var MAX = 24
+var conf_file = "reddit.conf"
 
 func print_headlines(count int, news bool) {
     if news {
@@ -23,14 +26,35 @@ func print_headlines(count int, news bool) {
 }
 
 func main() {
-    news := flag.Bool("n", false, "news flag")
+    dat, err := ioutil.ReadFile(conf_file)
+    if err != nil {
+        fmt.Printf("There was a problem reading the input file")
+    }
+
+    list := strings.Split(string(dat), "\n")
+    bool_flags := make([]*bool, 0)
+    urls := make([]string, 1)
+    
+    for _, line := range list {
+        item := strings.Split(line, " ")
+        // last line of the file? weird
+        if len(item) != 2 {
+            continue
+        }
+        url := item[1]
+        f := flag.Bool(item[0], false, "")
+        bool_flags = append(bool_flags, f)
+        urls = append(urls, url)
+    }
+
     count := flag.Int("c", 10, "count")
     flag.Parse()
+
     
     if *count > 24 {
         fmt.Printf("[!]: Count cannot exceed 24\n")
         return
     }
 
-    print_headlines(*count, *news)
+    // print_headlines(*count, *news)
 }
