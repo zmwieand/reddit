93545392265cdec170663f56bb383449d4f9932d
zmwieand <zmwieand@gmail.com>
2016-11-05 23:20:41 -0400

    making a print headlines function based on the command line arg

--- main.go
+++ main.go
@@ -6,6 +6,22 @@ import "github.com/PuerkitoBio/goquery"
 
 var MAX = 24
 
+func print_headlines(count int, news bool) {
+    if news {
+        doc, err := goquery.NewDocument("https://reddit.com/r/technology")
+        if err != nil {
+            fmt.Println("Whoops!")
+            return
+        }
+        doc.Find(".entry").Each(func(i int, s *goquery.Selection) {
+            if i < count {
+                title := s.Find("p .title").Text()
+                fmt.Printf("[%d]: %s\n\n",i,  title)
+            }
+        })
+    }
+}
+
 func main() {
     news := flag.Bool("n", false, "news flag")
     count := flag.Int("c", 10, "count")
@@ -14,20 +30,7 @@ func main() {
     if *count > 24 {
         fmt.Printf("[!]: Count cannot exceed 24\n")
         return
-    } 
-
-    if *news {
-        doc, err := goquery.NewDocument("https://reddit.com/r/news")
-        if err != nil {
-            fmt.Println("Whoops!")
-            return
-        }
-        doc.Find(".entry").Each(func(i int, s *goquery.Selection) {
-            if i < *count {
-                title := s.Find("p .title").Text()
-                fmt.Printf("[%d]: %s\n\n",i,  title)
-            }
-        })
-        
     }
+
+    print_headlines(*count, *news)
 }
